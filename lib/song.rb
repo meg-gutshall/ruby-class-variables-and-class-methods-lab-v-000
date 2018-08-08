4e80a5c1a9ceaeccaf7d6a568e26f148bc9784cf
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-08 14:07:01 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  attr_reader :genre_count_hash, :count, :genres, :artists, :n_songs
+  @@count = 0
+  @@genres = []
+  @@genre_count_hash = {}
+  @n_songs = 1
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.each do |g|
+      if @@genre_count_hash.key?(g)
+        @n_songs += 1
+      else
+        @@genre_count_hash[g] = @n_songs
+      end
+    end
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+end
